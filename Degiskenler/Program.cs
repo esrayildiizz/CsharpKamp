bcf8f43b03e4e8705ccd97520288d55eca57fe84
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 16:20:29 +0300

    Degiskenler duzenlendi.

--- Degiskenler/Program.cs
+++ Degiskenler/Program.cs
@@ -10,58 +10,45 @@ namespace ınt_değişkeni
     {
         static void Main(string[] args)
         {
-            //string ad, soyad, numara;
-            //int sınav1, sınav2, sınav3, proje, ortalama;
+            string ad, soyad, numara;
+            int sınav1, sınav2, sınav3, proje, ortalama;
 
-            //Console.WriteLine();
-            //Console.WriteLine("****Öğrenci Bilgi****");
-            //Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("****Öğrenci Bilgi****");
+            Console.WriteLine();
 
-            //ad = "Esra";
-            //soyad = "YILDIZ";
-            //numara = "123456";
+            ad = "Esra";
+            soyad = "YILDIZ";
+            numara = "123456";
 
-            //Console.WriteLine("AD= " + ad);
-            //Console.WriteLine("SOYAD= " + soyad);
-            //Console.WriteLine("NUMARA= " + numara);
+            Console.WriteLine("AD= " + ad);
+            Console.WriteLine("SOYAD= " + soyad);
+            Console.WriteLine("NUMARA= " + numara);
 
 
-            //Console.WriteLine();
-            //Console.WriteLine("****Not Bilgi****");
-            //Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("****Not Bilgi****");
+            Console.WriteLine();
 
-            //sınav1 = 60;
-            //sınav2 = 70;
-            //sınav3 = 100;
-            //proje = 90;
+            sınav1 = 60;
+            sınav2 = 70;
+            sınav3 = 100;
+            proje = 90;
 
-            //Console.WriteLine("Sınav1= " + sınav1);
-            //Console.WriteLine("Sınav2= " + sınav2);
-            //Console.WriteLine("Sınav3= " + sınav3);
-            //Console.WriteLine("Proje notu= " + proje);
+            Console.WriteLine("Sınav1= " + sınav1);
+            Console.WriteLine("Sınav2= " + sınav2);
+            Console.WriteLine("Sınav3= " + sınav3);
+            Console.WriteLine("Proje notu= " + proje);
 
-            //Console.WriteLine();
-            //Console.WriteLine("****Ortalama Bilgi****");
-            //Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("****Ortalama Bilgi****");
+            Console.WriteLine();
 
-            //ortalama = (sınav1 + sınav2 + sınav3 + proje) / 4;
-            //Console.WriteLine("Ortalama= " + ortalama);
-
-            //2.Proje
-
-            //int a = 15;
-            //int b = 15;
-            
-            //int c = 3;
-
-            //Console.WriteLine("a+b: " + (a+b));
-            //Console.WriteLine("a/c: " + a/c);
-
-            //3.Proje
+            ortalama = (sınav1 + sınav2 + sınav3 + proje) / 4;
+            Console.WriteLine("Ortalama= " + ortalama);
 
 
             Console.ReadLine();
-
 
         }
     }
---
+++ DoubleDegiskenler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleDegiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Öğrenci Not Hesaplama ");
+
+            double not1,not2,not3,proje,
+
+            Console.ReadLine();
+            
+        }
+    }
+}
