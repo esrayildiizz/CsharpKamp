2ce0cdbfc122787fe07c342d564db3ba1624d568
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 16:05:27 +0300

    Kare Alanı Bulma.

---
+++ AlanBulma/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlanBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kenar, karealan, karecevre;
+
+            Console.WriteLine("****Karenin Alanını Ve Çevresini Bulma****");
+            Console.WriteLine();
+
+            Console.WriteLine("Karenin Bir Kenarını Giriniz: ");
+         
+            kenar=Convert.ToInt32(Console.ReadLine());
+
+            karealan = kenar * kenar;
+            karecevre =kenar*4;
+
+            Console.WriteLine("Kare Alan: " + karealan);
+            Console.WriteLine("Kare Çevre: " + karecevre);
+
+            Console.ReadLine(  );
+            
+
+
+        }
+    }
+}
