0706140f37c438d6f6087b2570b860bfc8c7e923
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 23:34:58 +0300

    Düzenleme Yapıldı.

--- IfElseOrnekleri/Program.cs
+++ IfElseOrnekleri/Program.cs
@@ -50,6 +50,8 @@ namespace IfElseOrnek1
 
 
 
+
+
             //Proje3 (Klavyeden girilen sayı pozitif mi negatif mi?)
 
             //int girilensayı;
@@ -70,6 +72,8 @@ namespace IfElseOrnek1
 
 
 
+
+
             //Proje4 
 
             //int sınav1, sınav2, ort;
@@ -80,7 +84,7 @@ namespace IfElseOrnek1
             //Console.WriteLine("2.Sınav Notunuzu Giriniz");
             //sınav2= Convert.ToInt32(Console.ReadLine());
 
-            
+
             //ort = (sınav1 + sınav2) / 2;
             //Console.WriteLine("Ortalama: " + ort);
 
@@ -95,6 +99,191 @@ namespace IfElseOrnek1
             //}
 
             //Console.ReadLine();
+
+
+
+
+
+            //Proje5
+
+            //int not1, not2, proje, ort;
+
+            //Console.WriteLine("1.Sınavı Giriniz: ");
+            //not1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("2.Sınavı Giriniz: ");
+            //not2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Proje Notunu Giriniz: ");
+            //proje = Convert.ToInt32(Console.ReadLine());
+
+            //ort = (not1 + not2 + proje) / 3;
+            //Console.WriteLine("Ortamala: " + ort);
+
+            //if (ort>0 && ort<30)
+            //{
+            //    Console.WriteLine(" Durum: Çok Kötü ");
+            //}
+
+            //else if (ort>30 && ort<50)
+            //{
+            //    Console.WriteLine(" Durum: İyi Değil ");
+            //}
+
+            //else if (ort>=50 && ort<75)
+            //{
+            //    Console.WriteLine(" Durum: İyi ");    
+            //}
+
+            //else if (ort>=75 && ort < 90)
+            //{
+            //    Console.WriteLine(" Durum: Çok İyi ");
+            //}
+
+            //else if (ort>=90 && ort <=100)
+            //{
+            //    Console.WriteLine(" Durum: Süper ");
+            //}
+
+
+            //if (ort > 100)
+            //{
+            //    Console.WriteLine("Sınav Notlarında Hata Oluştu Lütfen Yeniden girin");
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+
+            //Proje6
+
+            //int sayi;
+
+            //Console.WriteLine("Sayıyı Giriniz: ");
+            //sayi=Convert.ToInt32(Console.ReadLine());
+
+
+            //if (sayi % 2==0)
+            //{
+            //    Console.WriteLine("Sayı Çiftir.");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Sayı Tektir.");
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+
+            //Proje7 (Klavyeden girilen sayı 5 e tam bölünür mü?)
+
+            //int sayi;
+            //Console.WriteLine(" Lütfen Sayıyı Giriniz: ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+
+            //if (sayi % 5==0)
+            //{
+            //    Console.WriteLine("Sayı 5'e Tam Bölünür.");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Sayı 5'e Tam Bölünmez.");
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+
+
+            //Proje8
+
+            //string mevsim;
+            //Console.WriteLine("Lütfen Bir Mevsim Giriniz.");
+            //mevsim = Console.ReadLine();
+
+            //if (mevsim=="kış")
+            //{
+            //    Console.WriteLine("Kış Mevsimi Ayları: Aralık-Ocak-Şubat ");
+            //}
+
+            //else if (mevsim == "ilkbahar")
+            //{
+            //    Console.WriteLine("İlkbahar Mevsimi Ayları: Mart-Nisan-Mayıs");
+            //}
+
+            //else if(mevsim =="yaz" )
+            //{
+            //    Console.WriteLine("Yaz Mevsimi Ayları: Haziran-Temmuz-Ağustos");
+            //}
+
+            //else if(mevsim=="sonbahar")
+            //{
+            //    Console.WriteLine("Sonbahar Mevsimi Ayları: Eylül-Ekim-Kasım");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Hatalı Mevsim Girişi Yaptınız.");
+            //}
+            //Console.ReadLine();
+
+
+
+
+
+
+            //Proje9
+
+            //int x, indirim, odenecek;
+            //Console.WriteLine("Alışveriş Tutarını Giriniz");
+            //x=Convert.ToInt32(Console.ReadLine());
+
+            //if (x < 100)
+            //{
+            //    indirim = x * 10 / 100;
+            //    odenecek = x - indirim;
+            //    Console.WriteLine("Ödenecek Tutar: " + odenecek);
+            //}
+
+            //if (x>=100 && x<150)
+            //{
+            //    indirim=x * 15 / 100;   
+            //    odenecek=x - indirim;
+            //    Console.WriteLine("Ödenecek Tutar: " + odenecek);
+            //}
+
+            //if(x>=150)
+            //{
+            //    indirim = x * 20 / 100;
+            //    odenecek = x - indirim;
+            //    Console.WriteLine("Ödenecek Tutar: " + odenecek);
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
