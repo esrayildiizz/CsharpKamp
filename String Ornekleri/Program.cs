8251dfe40357bfce7302c5a926b6fb2ec1f48320
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 14:31:55 +0300

    String ifadeler calısıldı.

---
+++ String Ornekleri/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Ornekleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad;
+            Console.WriteLine("Lütfen İsminizi Giriniz: ");
+            ad = Console.ReadLine();
+            Console.WriteLine("Merhaba: {0}", ad);
+            Console.Read();
+        }
+    }
+}
