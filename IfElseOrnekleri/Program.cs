144ebf131261778aa11d9cc79a2c600c9bb7e1b8
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 22:19:10 +0300

    If Else Ornekleri Yapıldı.

---
+++ IfElseOrnekleri/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseOrnek1
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Proje1
+
+            // string isim;
+            //Console.Write("İsim Giriniz: ");
+            // isim= Console.ReadLine();
+
+            //if (isim == "Esra")
+            //{
+            //    Console.WriteLine("Doğru İsim Girdiniz");
+            //} 
+            //else
+            //{
+            //    Console.WriteLine("Yanlış İsim Girdiniz");
+            //}
+            //Console.ReadLine();
+
+
+
+
+            //Proje2
+
+            //string sehir;
+            //Console.WriteLine("Türkiyenin Başkenti Neresidir");
+            //sehir= Console.ReadLine();
+
+            //if (sehir== "Ankara")
+
+            //{
+            //    Console.WriteLine("Tebrikler Doğru Bildiniz!");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Yanlış Cevap!");
+            //}
+
+            //Console.ReadLine();
+
+
+
+            //Proje3 (Klavyeden girilen sayı pozitif mi negatif mi?)
+
+            //int girilensayı;
+            //Console.WriteLine("Sayıyı Giriniz:");
+            //girilensayı = Convert.ToInt32(Console.ReadLine());
+
+            //if (girilensayı>0)
+            //{
+            //    Console.WriteLine("Pozitif Tam Sayı Girdiniz");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Negatif Tam Sayı Girdiniz");
+            //}
+
+            //Console.ReadLine();
+
+
+
+            //Proje4 
+
+            int sınav1, sınav2, ort;
+
+            Console.WriteLine("1.Sınav Notunuzu Giriniz");
+            sınav1= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("2.Sınav Notunuzu Giriniz");
+            sınav2= Convert.ToInt32(Console.ReadLine());
+
+            
+            ort = (sınav1 + sınav2) / 2;
+            Console.WriteLine("Ortalama: " + ort);
+
+            if (ort>50)
+            {
+                Console.WriteLine("Öğrenci Geçti :)");
+            }
+
+            else
+            {
+                Console.WriteLine("Öğrenci Kaldı :(");
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
