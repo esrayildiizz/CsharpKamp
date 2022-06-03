bb8d01e8a691e81aa5d0e02716c31f01829eb8a2
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 15:30:27 +0300

    Toplamaislemiyapıldı.

---
+++ DortIslem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DortIslem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı1, sayı2, toplam;
+
+            Console.WriteLine("****Toplama İşlemi****");
+
+            Console.WriteLine("Birinci Sayıyı Giriniz: ");
+            sayı1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İkinci Sayıyı Giriniz: ");
+            sayı2= Convert.ToInt32(Console.ReadLine());
+
+            toplam = sayı1 + sayı2;
+            Console.WriteLine( "Toplam: " +toplam);
+
+            Console.ReadLine();
+        }
+    }
+}
