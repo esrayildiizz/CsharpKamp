ada3b109ea3243b872f90912405ac651ad44b5c0
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 16:48:53 +0300

    Double degiskeni kullanıldı.

--- DoubleDegiskenler/Program.cs
+++ DoubleDegiskenler/Program.cs
@@ -12,7 +12,35 @@ namespace DoubleDegiskenler
         {
             Console.WriteLine(" Öğrenci Not Hesaplama ");
 
-            double not1,not2,not3,proje,
+            double not1, not2, not3, proje, ort;
+            string ad, soyad, numara;
+
+            Console.WriteLine("****Öğrenci Kimlik Bilgileri****");
+
+            Console.Write("Adınız: ");
+            ad = Console.ReadLine();
+            Console.Write("Soyadınız: ");
+            soyad= Console.ReadLine();  
+            Console.Write("Numaranız:");
+            numara = Console.ReadLine();
+
+            Console.WriteLine("****Öğrenci Sınav Bilgileri****");
+
+            Console.Write("Brinci Sınav: ");
+            not1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinci Sınav: ");
+            not2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Üçüncü Sınav:. ");
+            not3= Convert.ToDouble(Console.ReadLine());
+            Console.Write("Proje Notunuz. ");
+            proje = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("****Ortama****");
+            ort = (not1 + not2 + not3 + proje) / 4;
+            Console.WriteLine("Ad Soyad: " + ad + soyad);
+            
+         
+            Console.Write("Ortalamanız:  " + ort);
 
             Console.ReadLine();
             
