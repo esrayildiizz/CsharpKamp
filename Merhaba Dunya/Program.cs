600c24105b72d09ed8ac1b05aeb1071f7e378ffd
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 14:19:54 +0300

    İlk Projem

---
+++ Merhaba Dunya/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merhaba_Dunya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba C# Console Dersleri");
+            Console.Read();
+        }
+    }
+}
