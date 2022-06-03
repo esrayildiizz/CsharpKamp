48684908dd7e65259cab5dbf1dfe1b25d39e90e9
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-03 15:46:54 +0300

    Aritmetik Dort Islem Yapıldı.

--- DortIslem/Program.cs
+++ DortIslem/Program.cs
@@ -10,20 +10,35 @@ namespace DortIslem
     {
         static void Main(string[] args)
         {
-            int sayı1, sayı2, toplam;
-
-            Console.WriteLine("****Toplama İşlemi****");
+            int sayı1, sayı2, toplam, fark, carpim, bolum;
+            Console.WriteLine("****Aritmetik Dört İşlem****");
+            Console.WriteLine();
 
             Console.WriteLine("Birinci Sayıyı Giriniz: ");
-            sayı1 = Convert.ToInt32(Console.ReadLine());
+            sayı1= Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("İkinci Sayıyı Giriniz: ");
             sayı2= Convert.ToInt32(Console.ReadLine());
 
             toplam = sayı1 + sayı2;
-            Console.WriteLine( "Toplam: " +toplam);
+            Console.WriteLine("Toplam: " + toplam);
+            Console.WriteLine();
+
+            fark = sayı1 - sayı2;
+            Console.WriteLine("Fark: " + fark);
+            Console.WriteLine();
+
+            carpim = sayı1 * sayı2;
+            Console.WriteLine("Çarpım: " + carpim);
+            Console.WriteLine();
+
+            bolum = sayı1 / sayı2;
+            Console.WriteLine("Bölüm: " + bolum);
+          
 
             Console.ReadLine();
+
+
         }
     }
 }
