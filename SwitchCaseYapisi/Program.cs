d0b0d6770254ec33e74653709d56dd57295b3534
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-04 13:41:35 +0300

    SwitchCaseOrnekleriYapildi.

---
+++ SwitchCaseYapisi/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCaseYapisi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Proje1
+
+
+            //int sayi;
+            //Console.WriteLine("1 ile 7 arasında bir sayı giriniz: ");
+            //sayi= Convert.ToInt16(Console.ReadLine());
+
+            //switch(sayi)
+            //{
+
+            //    case 1: Console.WriteLine("Gün=Pazartesi");
+            //        break;
+            //    case 2: Console.WriteLine("Gün=Salı");
+            //        break;
+            //    case 3: Console.WriteLine("Gün=Çarşamba");
+            //        break;
+            //    case 4: Console.WriteLine("Gün=Perşembe");
+            //        break;
+            //    case 5: Console.WriteLine("Gün=Cuma");
+            //        break;
+            //    case 6: Console.WriteLine("Gün=Cumartesi");
+            //        break;
+            //    case 7: Console.WriteLine("Gün=Pazar");
+            //        break;  
+            //     default: Console.WriteLine("Hatalı Giriş Yptınız");
+            //        break;
+
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+
+
+            //Proje2 (Adana-Ankara-Antalya İlleri Arasından Seçim Yapılacaktır.)
+
+
+            //string sehir;
+            //Console.WriteLine("****İstanbul İline Uzaklığı Ne Kadar****");
+            //Console.WriteLine("Lütfen Şehir Giriniz: (Adana-Ankara-Antalya) ");
+            //sehir= Console.ReadLine();  
+
+            //switch (sehir)
+            //{
+            //    case "Adana": Console.WriteLine("İstanbul İlinin Adanaya'ya Uzaklığı 969 km'dir.");
+            //        break;
+            //    case "Ankara": Console.WriteLine("İstanbul İlinin Ankara'ya Uzaklığı 453 km'dir.");
+            //        break;
+            //    case "Antalya": Console.WriteLine("İstanbul İlinin Antalya'ya Uzaklığı 724 km'dir.");
+            //        break;
+            //    default: Console.WriteLine("Hatalı Giriş Yaptınız...");
+            //        break;
+
+            //}
+            //Console.ReadLine();
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
