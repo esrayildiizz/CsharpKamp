b1a1d122ac67f6f30a4ea40461b06c55bf73c01c
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-04 17:57:00 +0300

    Son Duzenleme Yapildi.

--- Donguler/Program.cs
+++ Donguler/Program.cs
@@ -172,6 +172,40 @@ namespace Donguler
 
 
 
+            //Proje10
+
+            //int sayi;
+            //Console.WriteLine("Faktöriyeli alınacak sayıyı girin: ");
+            //sayi=Convert.ToInt32(Console.ReadLine());
+
+            //int faktoriyel = 1;
+
+            //while (sayi > 1)
+            //{
+            //    faktoriyel = faktoriyel * sayi;
+            //    sayi--;
+            //}
+
+            //Console.WriteLine(faktoriyel);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
