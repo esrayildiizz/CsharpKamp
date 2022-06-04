82a577e2286142c29f0ce1e6366c787d212c50ae
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-04 22:33:45 +0300

    Ornekler Yapildi.

---
+++ Diziler/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Proje1
+
+            //int[] sayilar = { 10, 45, 21, 35, 64, 75, 84, 26 };
+
+            //Console.WriteLine(sayilar[0]); //sayilar adlı dizimden birinci elemanı getir demek
+
+            //Console.ReadLine();  
+
+
+
+
+
+            //Proje2
+
+            //string[] isimler = { "esra","ayse","veli","baran","hasan" };
+
+            //Console.WriteLine(isimler[3]);
+
+            //Console.ReadLine();
+
+
+
+
+
+            //Proje3
+
+            string[] sehirler = new string[3];
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    } 
+
+}
