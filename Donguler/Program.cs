466121c4dade5d3c4bb9e220b1831b9455d79b88
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-04 17:28:58 +0300

    Donguler ile ilgili ornekler yapıldı.

---
+++ Donguler/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Proje1 ( For Dongusu )
+
+            //int i;
+            //for(i=1; i<=10; i++)
+            //{
+            //    Console.WriteLine("Merhaba Dünya");   
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+            //Proje2
+
+            //int i;
+            //for(i=1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);   
+            //}
+
+            //Console.ReadLine();
+
+
+
+            //Proje3
+
+            //int i;
+            //for (i = 0; i <= 100; i+=10)
+            //{
+            //    Console.WriteLine(i);   
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+            //Proje4 
+
+            //int i;
+            //int toplam = 0;
+
+            //for (i=1; i<=100; i+=10)
+            //{
+            //    toplam= toplam+i;  
+            //}
+            //Console.WriteLine("Toplam: " +toplam);  
+
+
+            //Console.ReadLine();
+
+
+
+
+
+            //Proje5
+
+            //int i;
+            //for (i = 1; i <= 100; i++)
+            //{
+            //    if (i % 9 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+
+
+            //Proje6
+
+            //int sayi = 120;
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    if(sayi%i == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+
+
+
+            //Proje7
+
+            //int a = 1;
+            //int b = 1;
+            //int c;
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);   
+
+            //for(int i = 1; i <= 8; i++)
+            //{
+            //    c = a + b;
+            //    a = b;
+            //    b = c;
+            //    Console.WriteLine(c);
+            //}
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
