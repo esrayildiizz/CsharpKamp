9f9014695c7110e59df5655a9d8e8b30ca6455b1
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-04 23:03:06 +0300

    Ornek Eklendi.

--- Diziler/Program.cs
+++ Diziler/Program.cs
@@ -34,27 +34,63 @@ namespace Diziler
 
 
 
-            //Proje3
+            ////Proje3
 
-            string[] sehirler = new string[3];
+            //string[] sehirler = new string[3];
 
-            for(int i = 0; i <3 ; i++)
-            {
-                Console.WriteLine("Şehir: ");
-                sehirler[i] = Console.ReadLine();
-            }
-
-
-            Console.WriteLine("****Şehirler****");
-
-            for(int i = 0; i < 3; i++)
-            {
-                Console.WriteLine(sehirler[i]);
-            }
+            //for(int i = 0; i <3 ; i++)
+            //{
+            //    Console.WriteLine("Şehir: ");
+            //    sehirler[i] = Console.ReadLine();
+            //}
 
 
-            Console.ReadLine();
-                
+            //Console.WriteLine("****Şehirler****");
+
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(sehirler[i]);
+            //}
+
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+            //Proje4
+
+            //int[] sayilar = new int[5];
+
+            //for(int i= 0; i <5; i++)
+            //{
+            //    Console.WriteLine((i+1).ToString()+ " .Sayıyı Giriniz");
+            //    sayilar[i] = Convert.ToInt32(Console.ReadLine()); 
+            //}
+
+            //int enbuyuk;
+            //enbuyuk=sayilar[0];
+
+            //for(int i=1; i<5; i++)
+            //{
+            //    if (enbuyuk < sayilar[i])
+            //    {
+            //        enbuyuk = sayilar[i];
+            //    }
+               
+            //}
+            //Console.WriteLine("**** En Büyük Sayı****");
+
+            //Console.WriteLine(enbuyuk);
+
+            //Console.ReadLine();
+
+
+
+
 
 
 
