8863b2594990b7e817a91320b78804aa1ca5ea6f
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-05 10:54:10 +0300

    Baslangıc ornekleri eklendi.

---
+++ ForeachDongusu/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachDongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Proje1
+
+            //string[] sehirler = { "mersin", "izmir", "kocaeli", "artvin", "ankara", "erzurum", "şanlıurfa" };
+
+            //foreach (string i in sehirler)
+            //{
+            //    Console.WriteLine(i);   
+            //}
+
+            //Console.ReadLine(); 
+
+
+
+
+
+            //Proje2
+
+            //int[] sayilar = { 2, 6, 13, 26, 37, 56 };
+
+            //foreach(int i in sayilar)
+            //{
+            //    Console.WriteLine(i);   
+            //}
+
+
+            //Console.ReadLine(  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
