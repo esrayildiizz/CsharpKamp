7d282665ce026af0b5f18d45f51cfbf5d59a987d
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-06 17:58:29 +0300

    İlk ornek eklendi.

---
+++ DegıskenCesitleri/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegıskenCesitleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Proje1 //Decimal degiskeni
+
+            //Girilen sayıya 'nokta' koyarsak hata vermemesi için 'm' harfi koymalıyız.
+
+            //decimal sayi;
+            //sayi = 12.345m;
+            //Console.WriteLine(sayi);
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
