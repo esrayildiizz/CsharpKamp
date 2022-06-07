ae860e529e79a245c2dbf4c3dff67726f6490eac
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-07 13:56:55 +0300

    Son ornek eklendi.

--- DegıskenCesitleri/Program.cs
+++ DegıskenCesitleri/Program.cs
@@ -187,12 +187,21 @@ namespace DegıskenCesitleri
 
 
 
+            //Proje9  // bool degiskeninin iki sonucu vardır.
+            //True veya False dur. // {0} yazan yere true veya false gelecektir.
 
+            //int sayi;
+            //Console.WriteLine("Sayıyı Girin: ");
 
+            //sayi=Convert.ToInt32(Console.ReadLine());
 
+            //bool durum1 = sayi > 0;
+            //bool durum2 = sayi >10;
 
-
-
+            //Console.WriteLine("Sayı Pozitif Mi? : {0} ", durum1);
+            //Console.WriteLine("Sayı 10'dan Büyük Mü?: {0} " , durum2);
+           
+            //Console.ReadLine();
 
 
 
