31e650e1a68f419bbcb825cfb4aed279530e3d89
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-07 16:58:49 +0300

    Komutların yazılma sekli eklendi.

---
+++ ArkaPlanRengiDegistirme/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkaPlanRengiDegistirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Arka ekranı siyah olmasını istemediğimizde aşağıdaki komutlarla farklı bir renk yapabiliriz.
+
+            Console.BackgroundColor= ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("Nasılsınız.");
+            Console.ReadLine();
+
+
+            //Eğerki rengini değiştiğiniz  arka ekranın  yazı rengini de değişmek isterseniz aşağıdaki komutu ekleriz.
+
+
+
+
+
+
+
+        }
+    }
+}
