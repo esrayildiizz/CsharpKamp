906f17a70503a7682bd1448677378f56801e7046
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-08 21:45:38 +0300

    Mutlak deger alma komutları yazildi.

---
+++ Fonksiyonlar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fonksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Proje1 //Matematiksel Fonksiyonlar
+
+            // Mutlak değer alma.
+
+            int sayi;
+            Console.WriteLine("Sayıyı girin: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Mutlak Değerli Hali: " + Math.Abs(sayi));
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
