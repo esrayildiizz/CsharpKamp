633efbb0f585eecfbcd9fee514f0182a6e2a4062
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-08 14:30:29 +0300

    Klavyeden degerler girilerek matris olusturuldu.

--- CokBoyutluDiziler/Program.cs
+++ CokBoyutluDiziler/Program.cs
@@ -45,46 +45,75 @@ namespace CokBoyutluDiziler
             //Proje2
 
 
-            Console.WriteLine("Matrislerde Toplama");
+            //Console.WriteLine("Matrislerde Toplama");
 
-            int[,] matris1 = { { 10, 12, 16, 13 }, { 21, 25, 28, 23 }, { 45, 15, 19, 46 }, { 58, 86, 47, 69 } };
-            int[,] matris2 = { { 1, 2, 3, 4 }, { 25, 24, 28, 26 }, { 12, 6, 45, 36 }, { 9, 8, 7, 6 } };
-            int[,] toplam = new int[4, 4];
+            //int[,] matris1 = { { 10, 12, 16, 13 }, { 21, 25, 28, 23 }, { 45, 15, 19, 46 }, { 58, 86, 47, 69 } };
+            //int[,] matris2 = { { 1, 2, 3, 4 }, { 25, 24, 28, 26 }, { 12, 6, 45, 36 }, { 9, 8, 7, 6 } };
+            //int[,] toplam = new int[4, 4];
 
-            for (int i = 0; i < 4; i++)
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j=0; j < 4; j++)
+            //    {
+            //        toplam[i, j] = matris1[i, j] + matris2[i, j];
+            //    }
+
+            //}
+
+            //for (int k = 0; k < 4; k++)
+            //{
+            //    Console.WriteLine();
+            //    for(int m = 0; m < 4; m++)
+            //    {
+            //        Console.Write(toplam[k,m] + "  ");
+            //    }
+            //}
+
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+            //Proje3
+
+            int satir, sutun;
+            Console.WriteLine("Satır sayısını girin: ");
+            satir=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sütün sayısını girin: ");
+            sutun = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matris= new int[satir, sutun];   
+
+
+            for(int i = 0; i < satir; i++)
             {
-                for (int j=0; j < 4; j++)
+                for( int j=0; j < sutun; j++)
                 {
-                    toplam[i, j] = matris1[i, j] + matris2[i, j];
+                    Console.Write("Matrisin {0} x {1} Değeri: " , i+1,j+1);  //{0} satıra karşılık gelicek, {1} sütüna karşılık gelicek.
+                    matris[i,j] = Convert.ToInt32(Console.ReadLine());
                 }
 
             }
+            Console.WriteLine();
 
-            for (int k = 0; k < 4; k++)
+            for (int k = 0; k <satir; k++)
             {
-                Console.WriteLine();
-                for(int m = 0; m < 4; m++)
+                for(int n=0; n<sutun; n++)
                 {
-                    Console.Write(toplam[k,m] + "  ");
+                    Console.Write(matris[k,n]+ " " );
+               
                 }
+                Console.WriteLine();
             }
 
 
             Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
