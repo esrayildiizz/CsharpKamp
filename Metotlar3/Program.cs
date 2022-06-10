0ca3dbc5a13a30da3173681d48a8ca61be1e64a5
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-10 14:25:20 +0300

    Son duzenlemeler yapildi.

---
+++ Metotlar3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar3
+{
+    internal class Metotlar
+    {
+        int topla(int a, int b)
+        {
+            return a + b; //a ve b toplamını döndür.
+        }
+        static void Main(string[] args)
+        {
+            Metotlar mtr=new Metotlar(); //mtr adında  metotlar sınıfından türemiş bir nesne oluşturduk.
+            int z;
+            z = mtr.topla(1, 6);
+            Console.WriteLine("Toplam: " + z);
+            Console.ReadLine();
+        }
+    }
+}
