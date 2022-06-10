ad5d42c95d2644cfa00e6e3fdea7e30a2dbaa9e0
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-10 13:22:37 +0300

    Ornek eklendi.

---
+++ Metotlar2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar2
+{
+    internal class Program
+    {
+
+        //Proje1
+
+        private static void yazdir(string bilgi)
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(bilgi);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metni Girin: ");
+            string blg = Console.ReadLine();
+            yazdir(blg);
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
