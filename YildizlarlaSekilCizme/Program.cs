8380918a80129dcf4e87110b03658fd36b4b089e
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-10 12:46:08 +0300

    Ornek eklendi.

---
+++ YildizlarlaSekilCizme/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YildizlarlaSekilCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <=10 ; i++)
+            {
+                for (int j = 0; j < i ; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
