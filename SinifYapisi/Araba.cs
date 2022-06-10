0cc07bd3a9acd437d5d586c11a6f93959b5718c9
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-10 16:48:21 +0300

    Ornekler eklendi.

---
+++ SinifYapisi/Araba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinifYapisi
+{
+    internal class Araba
+    {
+        public string renk;
+        public int fiyat;
+        public string model;
+        public string vites;
+        public string plaka;
+        private int tekerkalite; //Özel olarak kullandığım bir erişim belirleyici.
+    }
+}
