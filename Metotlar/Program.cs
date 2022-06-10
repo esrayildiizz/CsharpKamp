008838777dcf86d6231ea1d3397e3ceb0370bf45
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-10 13:03:05 +0300

    ilk ornek eklendi.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        private static void veriler() //private gizli, erişilemez bilgi demektir.
+        {
+            Console.WriteLine("Müdür: Esra YILDIZ");
+            Console.WriteLine("Öğretmen: Tansu GEM");
+            Console.WriteLine("Okul: Atatürk Lisesi");
+            Console.WriteLine("Şehir: İstanbul");
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+        }
+        static void Main(string[] args)
+        {
+            veriler(); //veriler();ifadesini kaç kez yazarsam yukarıdaki veriler o kadar gelir.
+            Console.Read();
+        }
+    }
+}
