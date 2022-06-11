b2d5f42314510c73d1160cfc628797f213bdd907
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-11 14:15:15 +0300

    Ornek eklendi.

---
+++ Siniflar4/Yolcu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar4
+{
+    internal class Yolcu
+    {
+        private string ad;
+        private string soyad;
+        private int yas;
+        private string cinsiyet;
+
+        public string Ad
+        {
+            get { return ad; }  
+            set { ad = value.ToUpper(); } 
+        }
+        public string Soyad
+        {
+            get { return soyad; }   
+            set { soyad = value.ToUpper(); }
+        }
+        public int Yas
+        {
+            get { return yas; } 
+            set { yas = Math.Abs(value); }    
+        }
+        public string Cinsiyet
+        {
+            get { return cinsiyet; }    
+            set { cinsiyet = value.ToLower(); }   
+        }
+
+    }
+}
