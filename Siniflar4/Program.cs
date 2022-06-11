75fdd7ed2b9d03c014e580960709245bd0071de9
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-11 14:14:48 +0300

    Kalitim ornegi eklendi.

---
+++ Siniflar4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucak uck= new Ucak();
+            uck.Marka = "Türk Hava Yolları";
+            uck.Kalkis = "Adana-Şakirpaşa";
+            uck.Varis = "İstanbul-Sabiha Gökçen";
+            uck.Ad = "Esra";
+            uck.Soyad = "Yıldız";
+            uck.Yas = 24;
+            uck.Cinsiyet = "Kadın";
+
+            Console.WriteLine("Hava Yolu Şirketi: " + uck.Marka);
+            Console.WriteLine("Kalkış Yeri: " + uck.Kalkis);
+            Console.WriteLine("Varış Yeri: " + uck.Varis);
+            Console.WriteLine("Yolcu ADI: " + uck.Ad);
+            Console.WriteLine("Yolcu SOYADI: " + uck.Soyad);
+            Console.WriteLine("Yolcu Yaşı: " + uck.Yas);
+            Console.WriteLine("Yolcu Cinsiyeti: " + uck.Cinsiyet);
+
+            Console.Read();
+        }
+    }
+}
