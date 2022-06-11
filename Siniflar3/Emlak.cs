ef5b8c94b7858d487eca8fd61190d800051ede64
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-11 12:08:22 +0300

    Kapsulleme ornegi eklendi.

---
+++ Siniflar3/Emlak.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar3
+{
+    internal class Emlak
+    {
+
+        //Kapsülleme Örneği
+
+        private string semt;
+        private string renk;
+        private int odasayisi;
+        private int katno;
+        private double alan;
+
+        public string Semt
+        {
+            get { return semt; }
+            set { semt = value.ToUpper(); }   
+        }
+        public string Renk
+        {
+            get { return renk; }
+            set { renk = value.ToUpper(); } 
+        }
+        public int OdaSayısı
+        {
+            get { return odasayisi; }   
+            set { odasayisi = Math.Abs(value); }  
+        }
+        public int KatNo
+        {
+            get { return katno; }   
+            set { katno = Math.Abs(value); } 
+        }
+        public double Alan
+        {
+            get { return alan; }
+            set {  alan = Math.Abs(value); }    
+            
+        }
+        
+    }
+}
