5e0d4bcf7c34ad25f541bc05a5706a970bbf6007
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-11 11:34:00 +0300

    Ornek eklendi.

---
+++ Siniflar2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci(); //Ogrenci sınıfında ogr adlı bir nesne türettim.
+            ogr.Adı = "Esra";
+            ogr.Soyadı = "Yıldız";
+            ogr.Alanı = "Sayısal";
+            ogr.Yası = 17;
+
+            Console.WriteLine("Adı: " + ogr.Adı );
+            Console.WriteLine("Soyadı: " + ogr.Soyadı );
+            Console.WriteLine("Alanı: " + ogr.Alanı);
+            Console.WriteLine("Yaşı: " + ogr.Yası);
+
+            Console.Read();
+        }
+    }
+}
