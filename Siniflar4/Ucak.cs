6923db100cb4da43959842e20f02f00c3a5ab4ee
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-11 14:15:00 +0300

    Ornek eklendi.

---
+++ Siniflar4/Ucak.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar4
+{
+    internal class Ucak:Yolcu
+    {
+        private string marka;
+        private string kalkis;
+        private string varis;
+
+        public string Marka
+        {
+            get { return marka; }   
+            set { marka = value.ToUpper(); }
+        }
+
+        public string Kalkis
+        {
+            get { return kalkis; }  
+            set { kalkis = value.ToUpper(); }   
+        }
+
+        public string Varis
+        {
+            get { return varis; }   
+            set { varis = value.ToUpper(); }    
+        }
+    }
+}
