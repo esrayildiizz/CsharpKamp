6aa27b8774987d941d5df52212f99432bcec9f18
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-12 15:35:07 +0300

    Lig turnuvasi komutlari yazildi.

--- LigTurnuvasi/Program.cs
+++ LigTurnuvasi/Program.cs
@@ -41,7 +41,33 @@ namespace LigTurnuvasi
                 Console.WriteLine("-------------------------");
 
                 Console.WriteLine("GS " + a1 + " - " + b1 + " FB");
+                if (a1 > b1)
+                {
+                    gs = gs + 3;
+                }
+                if (b1 > a1)
+                {
+                    fb= fb + 3; 
+                }
+                if (a1 == b1)
+                {
+                    gs = gs + 1;
+                    fb= fb + 1; 
+                }
                 Console.WriteLine("BJK " + c1 + " - " + d1 + " TS \n");
+                if (c1 > d1)
+                {
+                    bjk = bjk + 3;
+                }
+                if (d1 > c1)
+                {
+                    ts = ts + 3;
+                }
+                if (c1 == d1)
+                {
+                    bjk = bjk + 1;
+                    ts = ts + 1;
+                }
                 sayac++;
                 Console.ReadLine();
             }
@@ -59,7 +85,33 @@ namespace LigTurnuvasi
                 Console.WriteLine("-------------------------");
 
                 Console.WriteLine("FB " + b2 + " - " +c2+ " BJK" );
+                if (b2 > c2)
+                {
+                    fb = fb + 3;
+                }
+                if (c2 > b2)
+                {
+                    bjk = bjk + 3;
+                }
+                if (b2 == c2)
+                {
+                    fb = fb + 1;
+                    bjk = bjk + 1;
+                }
                 Console.WriteLine( "GS " + a2+ " - " +d2+  " TS");
+                if (a2 > d2)
+                {
+                    gs = gs + 3;
+                }
+                if (d2 > a2)
+                {
+                    ts = ts + 3;
+                }
+                if (b2 == c2)
+                {
+                    gs = gs + 1;
+                    ts = ts + 1;
+                }
                 sayac++;
                 Console.ReadLine();
             }
@@ -79,15 +131,71 @@ namespace LigTurnuvasi
                 Console.WriteLine("-------------------------");
 
                 Console.WriteLine("FB " + b3 + " - " + d3 + " TS");
+                if (b3 > d3)
+                {
+                    fb = fb + 3;
+                }
+                if (d3 > b3)
+                {
+                    ts = ts + 3;
+                }
+                if (b3 == d3)
+                {
+                    fb = fb + 1;
+                    ts = ts + 1;
+                }
                 Console.WriteLine("GS " + a3 + " - " + c3 + " BJK");
+                if (a3 > c3)
+                {
+                    gs = gs + 3;
+                }
+                if (c3 > a3)
+                {
+                    bjk = bjk + 3;
+                }
+                if (a3 == c3)
+                {
+                    gs = gs + 1;
+                    bjk = bjk + 1;
+                }
                 sayac++;
                 Console.ReadLine();
             }
 
+            if(sayac == 4)
+            {
+                Console.WriteLine("****Maçlar Tamamlandı****\n");
+                Console.WriteLine("PUAN TABLOSU\n");
+                Console.WriteLine("GS: " + gs);
+                Console.WriteLine("FB: " +fb);
+                Console.WriteLine("BJK: " +bjk);
+                Console.WriteLine("TS: " +ts);
+            }
+            Console.WriteLine(); 
 
+            if (gs>fb && gs>bjk && gs>ts)
+            {
+                Console.WriteLine("**GALATASARAY ŞAMPİYON**");
+            }
 
+            if(fb>gs && fb>ts && fb>bjk)
+            {
+                Console.WriteLine("**FENERBAHÇE ŞAMPİYON**");
+            }
 
-
+            if(bjk>gs && bjk>fb && bjk>ts)
+            {
+                Console.WriteLine("**BEŞİKTAŞ ŞAMPİYON**");
+            }
+            if(ts>gs && ts>fb && ts>bjk)
+            {
+                Console.WriteLine("**TRABZONSPOR ŞAMPİYON**");
+            }
+            
+                
+                    
+         
+            Console.ReadLine();
         }
     }
 }
