3f4d31f3a77fc9c145333e7e14955f76cd1b57be
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-12 18:19:11 +0300

    Menu gorunumu olusturuldu.

---
+++ YemekSepeti/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekSepeti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double secim, adet, toplam = 0;
+            double hesap = 0;
+            Console.WriteLine();
+            Console.WriteLine("************************************ MENÜ *************************************\n");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("**          YEMEKLER                  **          İÇECEKLER                  **");
+            Console.WriteLine("**                                    **                                     **");
+            Console.WriteLine("**    1-Et Döner    - 20.00TL         **        8-Ayran    -  2.00 TL        **");
+            Console.WriteLine("**    2-Tavuk Döner - 10.00TL         **        9-Kola     -  5.00 TL        **");
+            Console.WriteLine("**    3-Tantuni     - 8.50 TL         **        10-Fanta   -  5.00 TL        **");
+            Console.WriteLine("**    4-Kokoreç     - 9.75 TL         **        11-Ice Tea -  6.75 TL        **");
+            Console.WriteLine("**    5-Börek       - 5.50 TL         **        12-Sprite  -  5.00 TL        **");
+            Console.WriteLine("**    6-Salata      - 7.25 TL         **        13-Çay     -  1.00 TL        **");
+            Console.WriteLine("**    7-Pide        - 9.50 TL         **        14-Su      -  0.50 TL        **");
+            Console.WriteLine("*******************************************************************************");
+            Console.Read();
+
+
+        }
+    }
+}
