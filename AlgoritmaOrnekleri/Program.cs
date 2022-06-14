bc8ba55fb78673ce3d53f80089c048c8661ecbfd
Esra YILDIZ <esrayildiizz@outlook.com>
2022-06-14 17:08:29 +0300

    Ornek eklendi.

---
+++ AlgoritmaOrnekleri/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmaOrnekleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Proje1-Parse Kullanımı
+            //Kullanıcının girdiği iki sayının karelerinin toplamını veren uygulama.
+
+            Console.WriteLine("Lütfen Birinci Sayıyı Giriniz...");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen İkinci Sayıyı Giriniz...");
+            int sayi2= int.Parse(Console.ReadLine());
+
+            int sonuc = (sayi1 * sayi1) + (sayi2 * sayi2);
+
+            Console.WriteLine("Sonuç: " + sonuc);
+
+            Console.ReadLine();
+        }
+    }
+}
